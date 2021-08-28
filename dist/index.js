39ce979a19f3179984cc1e3ac56bb48a5abed6c4
gouz7514 <gouz7514@gmail.com>
2021-08-28 16:34:35 +0900

    Update : basic types, array, tuple

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,18 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Baic Types
 let id = 5;
-console.log('ID : ', id);
+let name = 'Kim hak jae';
+let isHandsome = true;
+let x = 'Hello';
+let age;
+let arr = [1, 2, 3, 4, 5];
+let arr2 = [1, 2, 'hello', true];
+// Tuple
+let person = [1, 'Kim', true];
+// Tuple Array
+let employee;
+employee = [
+    [1, 'Kim'],
+    [2, 'Park']
+];
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
+// Baic Types
 let id: number = 5
+let name: string = 'Kim hak jae'
+let isHandsome: boolean = true
+let x: any = 'Hello'
+let age: number
 
-console.log('ID : ', id)
+let arr: number[] = [1,2,3,4,5]
+let arr2: any[] = [1,2,'hello',true]
+
+// Tuple
+let person: [number, string, boolean] = [1, 'Kim', true]
+// Tuple Array
+let employee: [number, string][]
+
+employee = [
+  [1, 'Kim'],
+  [2, 'Park']
+]
+
+export {}
