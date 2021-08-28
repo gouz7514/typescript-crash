efe1181eebb66cb2c8a9eb366ab0b3bf549e054b
gouz7514 <gouz7514@gmail.com>
2021-08-28 16:41:55 +0900

    Update : Union, Enum

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,7 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 // Baic Types
 let id = 5;
-let name = 'Kim hak jae';
+// let name: string = 'Kim hak jae'
 let isHandsome = true;
 let x = 'Hello';
 let age;
@@ -16,3 +15,24 @@ employee = [
     [1, 'Kim'],
     [2, 'Park']
 ];
+// Union
+let pid = 22;
+pid = '22';
+// Enum
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 1] = "Up";
+    Direction1[Direction1["Down"] = 2] = "Down";
+    Direction1[Direction1["Left"] = 3] = "Left";
+    Direction1[Direction1["Right"] = 4] = "Right";
+})(Direction1 || (Direction1 = {}));
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+console.log(Direction1.Up);
+console.log(Direction2.Left);
+// export {}
